849cbf57844c7ed7b5229653d961d03a2ce04e34
Ali A. Dhillon <aleedhx@gmail.com>
2020-07-12 17:22:42 +0500

    Descrease the top padding of main welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Weather App</title>
     <link rel="stylesheet" href="/css/main.css">
+    <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0" defer></script>
     <script src="/js/app.js" defer></script>
 </head>
 <body class="bg-gray-200">
-    <div id="app" class="flex justify-center pt-16">
+    <div id="app" class="flex justify-center pt-4">
         <weather-app></weather-app>
     </div>
 </body>
